83d45a30020f841154442124771fa7290c6b12ab
BlackPigeon <mihai.pricob@yahoo.com>
2024-11-12 10:53:24 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+
+const express = require("express");
+const port = process.env.PORT || 3000;
+const app = express();
+const mongoose = require("mongoose");
+
+// connect to mongo
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization",
+    );
+    next();
+});
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+// users router
+const usersRouter = require("./routes/usersBase");
+app.use("/v1/users", usersRouter);
+
+// users manager
+const usersRouterManager = require("./routes/usersManager");
+app.use("/v1/users/get", usersRouterManager);
+
+// home page
+app.use(express.static(__dirname + "/public"));
+app.get("/", (req, res) => {
+    //res.send("API is active!")
+    res.render("home");
+});
+
+app.listen(port, () => console.log("Server Started on localhost:" + port));
---
+++ routes/usersBase.js
@@ -0,0 +1,135 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+
+// Getting all
+// GET http://localhost:3000/v1/users/
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// Getting One
+// GET http://localhost:3000/v1/users/<id>
+router.get("/:id", getUser, (req, res) => {
+    res.send(res.user);
+});
+
+// Verify User
+// POST http://localhost:3000/v1/users/verify
+router.post("/verify", verifyuser, (req, res) => {
+    res.send(res.user);
+});
+
+// Creating one
+// POST http://localhost:3000/v1/users/
+router.post("/", async (req, res) => {
+    const user = new User({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+        address: req.body.address,
+        fiscal_code: req.body.fiscal_code,
+    });
+
+    try {
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Updating one
+// PATCH http://localhost:3000/v1/users/<id>
+router.patch("/:id", getUser, async (req, res) => {
+    if (req.body.name != null) {
+        res.user.name = req.body.name;
+    }
+    if (req.body.password != null) {
+        res.user.password = req.body.password;
+    }
+    if (req.body.email != null) {
+        res.user.email = req.body.email;
+    }
+    if (req.body.address != null) {
+        res.user.address = req.body.address;
+    }
+    if (req.body.fiscal_code != null) {
+        res.user.fiscal_code = req.body.fiscal_code;
+    }
+    try {
+        const updatedUser = await res.user.save();
+        res.json(updatedUser);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+});
+
+// Deleting One
+// DELETE http://localhost:3000/v1/users/<id>
+router.delete("/:id", getUser, async (req, res) => {
+    try {
+        await User.deleteOne({ _id: req.params.id });
+        res.json({ message: "Deleted user" });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+// middleware functions
+async function getUser(req, res, next) {
+    let user;
+    try {
+        user = await User.findById(req.params.id);
+        if (user == null) {
+            return res.status(404).json({ message: "Cannot find user" });
+        }
+    } catch (err) {
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.user = user;
+    next();
+}
+
+async function verifyuser(req, res, next) {
+    if (req.body.email != null && req.body.password != null) {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        let user;
+
+        try {
+            user = await User.findOne({
+                email: email,
+                password: password,
+            }).exec();
+            if (!user) {
+                return res.status(404).json({ message: "false" });
+            }
+        } catch (err) {
+            return res.status(505).json({ message: err.message });
+        }
+
+        return res.status(200).json({ message: "true" });
+        next();
+    } else {
+        if (req.body.email == null) {
+            return res
+                .status(400)
+                .json({ message: "you must provide an email" });
+        }
+        if (req.body.password == null) {
+            return res
+                .status(400)
+                .json({ message: "you must provide an password" });
+        }
+    }
+}
+
+module.exports = router;
